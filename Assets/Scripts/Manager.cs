08a5761dd6ae102cdcd9edc07b73f15d0109eaf6
transo <strinhanh@gmail.com>
2024-01-27 11:04:43 +0700

    t

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Photon.Pun;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
@@ -10,23 +11,17 @@ public class Manager : MonoBehaviour
 
     public GameObject playerPrefab;
 
+    public GameObject fartPrefab;
+
     public Joystick joystick;
 
-    public float minX, maxX, minY, maxY;
+    public Button attackButton;
 
-    public int playerCount;
-
-    public TextMeshProUGUI timeTextMesh;
-
-    public float Countdown = 5.0f;
+    public float minX, maxX, minZ, maxZ;
 
     public SimpleController player;
 
     public SimpleController enemy;
-
-    private bool isTimerRunning;
-
-    private int startTime;
 
     private void Awake()
     {
@@ -40,8 +35,18 @@ public class Manager : MonoBehaviour
 
     private void Start()
     {
-        Vector2 randomPosition = new(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        Vector3 randomPosition = new(Random.Range(minX, maxX), 5, Random.Range(minZ, maxZ));
 
         PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { Attack(); }
+    }
+
+    public void Attack()
+    {
+        PhotonNetwork.Instantiate(fartPrefab.name, new Vector3(player.transform.position.x, 0.5f, player.transform.position.z), Quaternion.identity).transform.forward = new Vector3(joystick.Direction.x, 0, joystick.Direction.y);
     }
 }
